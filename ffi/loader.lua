47662a7a0fbac01ab827c8e37dbfcb0665f76477
fesily <fesil@foxmail.com>
2022-11-02 13:10:38 +0800

    first commit

---
+++ ffi/loader.lua
@@ -0,0 +1,69 @@
+local ffi = require 'ffi'
+local string_gmatch = string.gmatch
+local string_match = string.match
+local old_ffi_load = ffi.load
+local _M = {
+    is_ios = ffi.os == 'ios'
+}
+---@param name string
+function _M.loadlib(name, global, cpath)
+    local so_name = name
+
+    for k, _ in string_gmatch(cpath, "[^;]+") do
+        local so_path = string_match(k, "(.*/)")
+        if so_path then
+            -- "so_path" could be nil. e.g, the dir path component is "."
+            so_path = so_path .. so_name
+
+            -- Don't get me wrong, the only way to know if a file exist is
+            -- trying to open it.
+            local f = io.open(so_path)
+            if f ~= nil then
+                io.close(f)
+                return ffi.load(so_path, global)
+            end
+        end
+    end
+end
+
+local function load(name, global, cpath)
+    local lib = _M.loadlib(name, global, cpath)
+    if lib then return lib end
+
+    local is_ios = _M.is_ios
+
+    local b = name:find(".so")
+    if not b then
+        lib = _M.loadlib(name .. '.so', global, cpath)
+        if lib then return lib end
+
+        if is_ios then
+            lib = _M.loadlib(name .. '.dylib', global, cpath)
+            if lib then return lib end
+        end
+    else
+        if is_ios then
+            lib = _M.loadlib(name:sub(1, b - 1) .. '.dylib', global, cpath)
+            if lib then return lib end
+        end
+    end
+end
+
+---@param name    string
+---@param global? boolean
+---@param find_lua_path? boolean
+---@return ffi.namespace* clib
+---@nodiscard
+function ffi.load(name, global, find_lua_path)
+    local lib = load(name, global, package.cpath)
+    if lib then return lib end
+
+    if find_lua_path then
+        lib = load(name, global, package.path)
+        if lib then return lib end
+    end
+
+    return old_ffi_load(name, global)
+end
+
+return _M
